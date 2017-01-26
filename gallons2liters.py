8f534a99125dbb7fc16208e8bcd760a8655b2929
sananta <sananta@ucdavis.edu>
2017-01-25 20:06:50 -0800

    Create gallons2liters.py

---
+++ gallons2liters.py
@@ -0,0 +1,10 @@
+## prog1.py
+## Srivarshini Ananta
+## January 10, 2016
+##
+## convert U.S. liquid gallons to liters
+
+gallonsTOliters = 3.78541
+gallons = 20
+liters = gallons * gallonsTOliters
+print(gallons,' gallons is ',liters,' liters')
